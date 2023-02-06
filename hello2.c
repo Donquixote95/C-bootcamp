c8bde5540ff3381c7bc88be86a0e00e6b43067aa
LimDoeon <doeonlim8656@gmail.com>
2023-02-06 21:35:46 +0900

    v1

---
+++ hello2.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // Preprocessor directive
+#define MY_CONSTANT 10
+int main(void) 
+{
+    printf("You defined a fixed value %d\n", MY_CONSTANT);
+    printf("Hello C!\n");
+    printf("Good Bye C!\n");
+    return 0;
+} 
+
+// 코멘드 다 달기
+
+/* 
+코멘트 열고 닫기 
+멀티 라인으로도 달 수 있다.
+*/
