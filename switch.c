3bf99042fd1dbfd922140ecbfa2fcc26732d35b0
LimDoeon <doeonlim8656@gmail.com>
2023-02-08 18:53:10 +0900

    v1

---
+++ switch.c
@@ -0,0 +1,10 @@
+// Similar to multiple if-else statements but uses "case"
+/*
+Importatnt to include break; at the end of each case
+
+Case is only a starting point of execution, not the end of it.
+
+Without break, all the remaining codes in the switch statement will be executed
+
+default case can be optionally included
+*/
